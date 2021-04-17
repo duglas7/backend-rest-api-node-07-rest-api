d65ff4e9eadc2c67ce36491b53903549b0759e49
Duglas Medina <duglasmedina@gmail.com>
2021-04-16 23:15:01 -0400

    hasta la clase de cors

---
+++ models/server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    // Middlewares son funciones que se van a ejecutar cuando levantemos el servidor dan mas funcionalidad al codigo
+    this.middelwares();
+
+    // Rutas de mi aplicación
+    this.routes();
+  }
+
+  middelwares() {
+    // CORS
+    this.app.use(cors());
+
+    // Directorio Público
+    this.app.use(express.static("public"));
+  }
+
+  routes() {
+    this.app.get("/api", (req, res) => {
+      res.json({
+        msg: "get API",
+      });
+    });
+
+    this.app.put("/api", (req, res) => {
+      res.json({
+        msg: "put API",
+      });
+    });
+
+    this.app.post("/api", (req, res) => {
+      res.status(201).json({
+        msg: "post API",
+      });
+    });
+
+    this.app.delete("/api", (req, res) => {
+      res.json({
+        msg: "delete API",
+      });
+    });
+
+    this.app.patch("/api", (req, res) => {
+      res.json({
+        msg: "patch API",
+      });
+    });
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Servidor corriendo en puerto", this.port);
+    });
+  }
+}
+module.exports = Server;
